e8ea0cec6ce163262ccd0934a775bd19eed85c25
Shalini2517 <shalinir643@gmail.com>
2025-02-18 16:14:53 +0530

    Rest api practices

--- src/apiServices/axiosApi.jsx
+++ src/apiServices/axiosApi.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export const AxiosApi = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    apiIntegration();
+  }, []);
+
+  const apiIntegration = () => {
+    axios
+      .get(url)
+      .then((data) => {
+        console.log(data);
+        if (data.status == 200) {
+          setData(data.data);
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("ERROR :" + error);
+        setLoading(false);
+      });
+  };
+
   return (
     <>
       <h5>Axios Api</h5>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {data.length === 0 ? (
+            <p>No Data Found</p>
+          ) : (
+            <ul>
+              {data.map((value) => (
+                <li key={value.id}>User-name is {value.name}</li>
+              ))}
+            </ul>
+          )}
+        </>
+      )}
     </>
   );
 };
--- src/apiServices/promiseAsyncAwait.jsx
+++ src/apiServices/promiseAsyncAwait.jsx
@@ -1,7 +1,41 @@
+import { useState, useEffect, useLayoutEffect } from "react";
+
 export const PromiseApi = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    restApiWithAsync();
+  }, []);
+
+  const restApiWithAsync = async () => {
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      console.log(result);
+      setUsers(result);
+    } catch (error) {
+      console.error("Error : " + error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <h5>Promise Api</h5>
+      {loading ? (
+        <p>Loading...</p>
+      ) : users.length === 0 ? (
+        <p>No Users found</p>
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>User Name : {user.name}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
